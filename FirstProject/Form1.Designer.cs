d5a55a30826bc364c67dae20755d38ffab8f7b6a
DanielKork <101044386+DanielKork@users.noreply.github.com>
2022-04-26 18:00:34 +0300

    1

--- FirstProject/Form1.Designer.cs
+++ FirstProject/Form1.Designer.cs
@@ -46,7 +46,7 @@ namespace FirstProject
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(762, 50);
+            this.button1.Location = new System.Drawing.Point(808, 476);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(94, 29);
             this.button1.TabIndex = 1;
