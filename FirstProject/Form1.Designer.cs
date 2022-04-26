6ad77838390c3b173dff84de45157816e9651e2c
DanielKork <101044386+DanielKork@users.noreply.github.com>
2022-04-26 15:57:02 +0300

    snake

---
+++ FirstProject/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿
+namespace FirstProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.snake = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // snake
+            // 
+            this.snake.AutoSize = true;
+            this.snake.BackColor = System.Drawing.Color.Transparent;
+            this.snake.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.snake.Image = global::FirstProject.Properties.Resources.snake;
+            this.snake.Location = new System.Drawing.Point(158, 245);
+            this.snake.Name = "snake";
+            this.snake.Size = new System.Drawing.Size(0, 31);
+            this.snake.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FirstProject.Properties.Resources.snake;
+            this.pictureBox1.Location = new System.Drawing.Point(601, 228);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 62);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 517);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.snake);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label snake;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ FirstProject/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //label1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 'w')
+            {
+                //label1.Top -= 100;
+                pictureBox1.Top -= 10;
+            }
+            if (e.KeyChar == 's')
+            {
+                //label1.Top += 100;
+                pictureBox1.Top += 10;
+            }
+            if (e.KeyChar == 'd')
+            {
+                pictureBox1.Left += 10;
+            }
+            if (e.KeyChar == 'a')
+            {
+                pictureBox1.Left -= 10;
+            }
+        }
+    }
+}
