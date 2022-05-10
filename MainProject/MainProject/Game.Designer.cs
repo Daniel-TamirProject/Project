4d1e56e760a2049fc59d0d9d1b5e42bf864e28be
DanielKork <101044386+DanielKork@users.noreply.github.com>
2022-05-10 10:17:43 +0300

    newpanel

--- MainProject/MainProject/Game.Designer.cs
+++ MainProject/MainProject/Game.Designer.cs
@@ -29,184 +29,17 @@ namespace MainProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel8 = new System.Windows.Forms.Panel();
-            this.panel4 = new System.Windows.Forms.Panel();
-            this.panel5 = new System.Windows.Forms.Panel();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.panel6 = new System.Windows.Forms.Panel();
-            this.panel7 = new System.Windows.Forms.Panel();
-            this.button6 = new System.Windows.Forms.Button();
-            this.button7 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.panel1.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel2.SuspendLayout();
-            this.panel4.SuspendLayout();
-            this.panel5.SuspendLayout();
-            this.panel6.SuspendLayout();
-            this.panel7.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.backgroundpanel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // panel1
+            // backgroundpanel
             // 
-            this.panel1.BackColor = System.Drawing.Color.Transparent;
-            this.panel1.Controls.Add(this.panel3);
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Location = new System.Drawing.Point(3, 2);
-            this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(513, 287);
-            this.panel1.TabIndex = 0;
-            this.panel1.BackColorChanged += new System.EventHandler(this.panel1_BackColorChanged);
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            this.panel1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseClick);
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.Transparent;
-            this.panel3.Controls.Add(this.button3);
-            this.panel3.Location = new System.Drawing.Point(3, 318);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(493, 222);
-            this.panel3.TabIndex = 2;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(229, 134);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 1;
-            this.button3.Text = "test";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Visible = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(229, 134);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "test";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Visible = false;
-            // 
-            // panel2
-            // 
-            this.panel2.BackColor = System.Drawing.Color.Transparent;
-            this.panel2.Controls.Add(this.pictureBox1);
-            this.panel2.Controls.Add(this.panel8);
-            this.panel2.Location = new System.Drawing.Point(522, 2);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(542, 287);
-            this.panel2.TabIndex = 1;
-            this.panel2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel2_MouseClick);
-            // 
-            // panel8
-            // 
-            this.panel8.BackgroundImage = global::MainProject.Properties.Resources.cow2;
-            this.panel8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.panel8.Location = new System.Drawing.Point(472, 10);
-            this.panel8.Name = "panel8";
-            this.panel8.Size = new System.Drawing.Size(61, 52);
-            this.panel8.TabIndex = 2;
-            this.panel8.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel8_MouseClick);
-            // 
-            // panel4
-            // 
-            this.panel4.BackColor = System.Drawing.Color.Transparent;
-            this.panel4.Controls.Add(this.panel5);
-            this.panel4.Controls.Add(this.button5);
-            this.panel4.Location = new System.Drawing.Point(522, 295);
-            this.panel4.Name = "panel4";
-            this.panel4.Size = new System.Drawing.Size(542, 247);
-            this.panel4.TabIndex = 2;
-            this.panel4.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel4_MouseClick);
-            // 
-            // panel5
-            // 
-            this.panel5.BackColor = System.Drawing.Color.Transparent;
-            this.panel5.Controls.Add(this.button4);
-            this.panel5.Location = new System.Drawing.Point(3, 318);
-            this.panel5.Name = "panel5";
-            this.panel5.Size = new System.Drawing.Size(493, 222);
-            this.panel5.TabIndex = 2;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(229, 134);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 1;
-            this.button4.Text = "test";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Visible = false;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(229, 134);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 1;
-            this.button5.Text = "test";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Visible = false;
-            // 
-            // panel6
-            // 
-            this.panel6.BackColor = System.Drawing.Color.Transparent;
-            this.panel6.Controls.Add(this.panel7);
-            this.panel6.Controls.Add(this.button7);
-            this.panel6.Location = new System.Drawing.Point(3, 295);
-            this.panel6.Name = "panel6";
-            this.panel6.Size = new System.Drawing.Size(513, 247);
-            this.panel6.TabIndex = 3;
-            this.panel6.MouseClick += new System.Windows.Forms.MouseEventHandler(this.panel6_MouseClick);
-            // 
-            // panel7
-            // 
-            this.panel7.BackColor = System.Drawing.Color.Transparent;
-            this.panel7.Controls.Add(this.button6);
-            this.panel7.Location = new System.Drawing.Point(3, 318);
-            this.panel7.Name = "panel7";
-            this.panel7.Size = new System.Drawing.Size(493, 222);
-            this.panel7.TabIndex = 2;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(229, 134);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 1;
-            this.button6.Text = "test";
-            this.button6.UseVisualStyleBackColor = true;
-            this.button6.Visible = false;
-            // 
-            // button7
-            // 
-            this.button7.Location = new System.Drawing.Point(229, 134);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(75, 23);
-            this.button7.TabIndex = 1;
-            this.button7.Text = "test";
-            this.button7.UseVisualStyleBackColor = true;
-            this.button7.Visible = false;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::MainProject.Properties.Resources.cow2;
-            this.pictureBox1.Location = new System.Drawing.Point(29, 171);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(110, 81);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.pictureBox1.TabIndex = 3;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Visible = false;
+            this.backgroundpanel.BackColor = System.Drawing.Color.Transparent;
+            this.backgroundpanel.Location = new System.Drawing.Point(3, 3);
+            this.backgroundpanel.Name = "backgroundpanel";
+            this.backgroundpanel.Size = new System.Drawing.Size(1061, 495);
+            this.backgroundpanel.TabIndex = 0;
+            this.backgroundpanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.backgroundpanel_MouseClick);
             // 
             // Game
             // 
@@ -215,42 +48,17 @@ namespace MainProject
             this.BackgroundImage = global::MainProject.Properties.Resources.grassBackground2;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1067, 554);
-            this.Controls.Add(this.panel6);
-            this.Controls.Add(this.panel4);
-            this.Controls.Add(this.panel2);
-            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.backgroundpanel);
             this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Game";
             this.Text = "Game";
             this.Load += new System.EventHandler(this.Game_Load);
-            this.panel1.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
-            this.panel4.ResumeLayout(false);
-            this.panel5.ResumeLayout(false);
-            this.panel6.ResumeLayout(false);
-            this.panel7.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Panel panel4;
-        private System.Windows.Forms.Panel panel5;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Panel panel6;
-        private System.Windows.Forms.Panel panel7;
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Panel panel8;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel backgroundpanel;
     }
 }
--- MainProject/MainProject/Game.cs
+++ MainProject/MainProject/Game.cs
@@ -24,43 +24,7 @@ namespace MainProject
             
         }
 
-        private void panel1_BackColorChanged(object sender, EventArgs e)
-        {
-            
-        }
-
-        private void panel1_Paint(object sender, PaintEventArgs e)
-        {
-            
-        }
-
-        private void panel1_MouseClick(object sender, MouseEventArgs e)
-        {
-            button1.Visible = true;
-        }
-
-        private void panel2_MouseClick(object sender, MouseEventArgs e)
-        {
-            //PictureBox cow = new PictureBox();
-            PictureBox cow = pictureBox1;
-            cow.Visible = true;
-            //cow.Location = New point(e.Location.X, e.Location.Y);
-            //cow.BackgroundImage = "C:\Users\danie\Documents\GitHub\Project\MainProject\MainProject\Resources\cow2.jpg";
-            //cow.BackgroundImage = Image.FromFile(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal)
-            //    + @"C:\Users\danie\Documents\GitHub\Project\MainProject\MainProject\Resources\cow2.jpg");
-        }
-
-        private void panel4_MouseClick(object sender, MouseEventArgs e)
-        {
-            button5.Visible = true;
-        }
-
-        private void panel6_MouseClick(object sender, MouseEventArgs e)
-        {
-            button7.Visible = true;
-        }
-
-        private void panel8_MouseClick(object sender, MouseEventArgs e)
+        private void backgroundpanel_MouseClick(object sender, MouseEventArgs e)
         {
 
         }
