99b91caf800b7bfea3cd2725a51b4a315d2097f9
DanielKork <101044386+DanielKork@users.noreply.github.com>
2022-05-15 11:25:15 +0300

    Revert "2"
    This reverts commit 437a6e1b8acd1648e49962d7beecde44e208d574.

--- MainProject/MainProject/product.cs
+++ MainProject/MainProject/product.cs
@@ -3,21 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Windows.Forms;
 
 namespace MainProject
 {
-    class Product
+    class product
     {
         public int type;
-        public PictureBox photo;
-
-        public Product(int ty)
-        {
-            
-        }
     }
 }
