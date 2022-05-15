9da47099886ef9623708639bb8bc5bb1c3b458fa
DanielKork <101044386+DanielKork@users.noreply.github.com>
2022-05-15 11:30:20 +0300

    animal constructor

--- MainProject/MainProject/Animal.cs
+++ MainProject/MainProject/Animal.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace MainProject
 {
@@ -11,6 +15,35 @@ namespace MainProject
         public int breed; //{ 1 = Cow, 2 = Chicken }
         public String name;
         public static int numberOfAnimal;
-        
+
+        public PictureBox photo;
+
+        public Animal(int bre, string na)
+        {
+            breed = bre;
+            name = na;
+            if (bre == 1)
+            {
+                photo.Name = "cow";
+                photo.Width = 50;
+                photo.Height = 50;
+
+                photo.Image = MainProject.Properties.Resources.cow2_removebg_preview;
+                //picture1.Image = Image.FromFile(@"C:\Users\danie\Documents\GitHub\Project\MainProject\MainProject\Resources\cow2.jpg");
+                photo.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else if (bre == 2)
+            {
+                photo.Name = "duck";
+                photo.Width = 50;
+                photo.Height = 50;
+
+                photo.Image = MainProject.Properties.Resources.cow2_removebg_preview;
+                //picture1.Image = Image.FromFile(@"C:\Users\danie\Documents\GitHub\Project\MainProject\MainProject\Resources\cow2.jpg");
+                photo.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+
     }
 }
