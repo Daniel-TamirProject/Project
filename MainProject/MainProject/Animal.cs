f31f0f2a091cd127dba6973f79c796ea43d2571f
DanielKork <101044386+DanielKork@users.noreply.github.com>
2022-05-15 15:38:03 +0300

    1

--- MainProject/MainProject/Animal.cs
+++ MainProject/MainProject/Animal.cs
@@ -18,31 +18,31 @@ namespace MainProject
 
         public PictureBox photo;
 
-        public Animal(int bre, string na)
-        {
-            breed = bre;
-            name = na;
-            if (bre == 1)
-            {
-                photo.Name = "cow";
-                photo.Width = 50;
-                photo.Height = 50;
+        //public Animal(int bre, string na)
+        //{
+        //    breed = bre;
+        //    name = na;
+        //    if (bre == 1)
+        //    {
+        //        photo.Name = "cow";
+        //        photo.Width = 50;
+        //        photo.Height = 50;
 
-                photo.Image = MainProject.Properties.Resources.cow2_removebg_preview;
-                //picture1.Image = Image.FromFile(@"C:\Users\danie\Documents\GitHub\Project\MainProject\MainProject\Resources\cow2.jpg");
-                photo.SizeMode = PictureBoxSizeMode.StretchImage;
-            }
-            else if (bre == 2)
-            {
-                photo.Name = "duck";
-                photo.Width = 50;
-                photo.Height = 50;
+        //        photo.Image = MainProject.Properties.Resources.cow2_removebg_preview;
+        //        //picture1.Image = Image.FromFile(@"C:\Users\danie\Documents\GitHub\Project\MainProject\MainProject\Resources\cow2.jpg");
+        //        photo.SizeMode = PictureBoxSizeMode.StretchImage;
+        //    }
+        //    else if (bre == 2)
+        //    {
+        //        photo.Name = "duck";
+        //        photo.Width = 50;
+        //        photo.Height = 50;
 
-                photo.Image = MainProject.Properties.Resources.cow2_removebg_preview;
-                //picture1.Image = Image.FromFile(@"C:\Users\danie\Documents\GitHub\Project\MainProject\MainProject\Resources\cow2.jpg");
-                photo.SizeMode = PictureBoxSizeMode.StretchImage;
-            }
-        }
+        //        photo.Image = MainProject.Properties.Resources.cow2_removebg_preview;
+        //        //picture1.Image = Image.FromFile(@"C:\Users\danie\Documents\GitHub\Project\MainProject\MainProject\Resources\cow2.jpg");
+        //        photo.SizeMode = PictureBoxSizeMode.StretchImage;
+        //    }
+        //}
 
 
     }
