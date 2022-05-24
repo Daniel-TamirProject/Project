1437bc1b9fff0a032031da70e9c581d624e36855
DanielKork <101044386+DanielKork@users.noreply.github.com>
2022-05-24 10:52:01 +0300

    1

--- MainProject/MainProject/Game.cs
+++ MainProject/MainProject/Game.cs
@@ -123,7 +123,7 @@ namespace MainProject
 
         private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
-            for(int i=0;i<products.Count;i++)
+            for (int i = 0; i < products.Count; i++)
             {
                 products[i].Draw(e.Graphics);
             }
